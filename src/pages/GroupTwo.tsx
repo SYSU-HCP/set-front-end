12c79a72fff8c24c7817fd672150bcfd8bb7ea82
Juliiii <zhulf6@mail2.sysu.edu.cn>
2018-07-12 10:54:53 +0800

    save

---
+++ src/pages/GroupTwo.tsx
@@ -0,0 +1,21 @@
+// import * as React from 'react';
+// import videojs from 'video.js';
+// export default class GroupTwo extends React.PureComponent<any, any> {
+
+//   public constructor(props:any) {
+//     super(props)
+//   }
+
+//   public componentDidMount() {
+
+//   }
+
+//   public render() {
+//     return (
+//       <div>
+//         <video>
+//         </video>
+//       </div>
+//     )
+//   }
+// }
